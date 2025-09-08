50cabc0bf369d1c6ad7bf37ec61812af503c6e91
ShimanshuChauhan <shivanshuchauhan699@gmail.com>
2025-09-08 11:56:00 +0530

    chore : added for loop

--- hello.js
+++ hello.js
@@ -1,2 +1,6 @@
 console.log("Fix conflict with Sami ANAND");
 console.log("Complaint send to Harpal Thetti Ji");
+/*Added by Shimanshu Chauhan*/
+for (var i = 0; i <= 10; i++) {
+  console.log("hello");
+}
